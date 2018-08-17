5c1654fd6e41719d9026996428b641ef4d6a3874
sujunfeng <76686094@qq.com>
2018-08-17 18:42:29 +0800

    cookie, 分页

--- app01/views.py
+++ app01/views.py
@@ -84,3 +84,25 @@ def cookie(request):
     rep = render(request, 'index.html')
     rep.set_cookie(key, value)
     rep.set_signed_cookie(key, value, salt='加密盐')
+    # 设置cookie N秒后时效  秒为单位
+    rep.set_cookie('username11', 'value', max_age=10)
+    # 设置cookie 截止时间生效
+    import datetime
+    current_date = datetime.datetime.utcnow()
+    current_date = current_date + datetime.timedelta(second=5)
+    rep.set_cookie('username111', 'value', expires=current_date)
+
+    # 做一个checkbox,选中值，true，把选中值换算成时间，传入设置cookie，就可以实现多久时间免登陆
+
+    #     参数：
+    #     key,              键
+    #     value='',         值
+    #     max_age=None,     超时时间
+    #     expires=None,     超时时间(IE requires expires, so set it if hasn't been already.)
+    #     path='/',         Cookie生效的路径，/ 表示根路径，特殊的：跟路径的cookie可以被任何url的页面访问
+    #     domain=None,      Cookie生效的域名
+    #     secure=False,     https传输
+    #     httponly=False    只能http协议传输，无法被JavaScript获取（不是绝对，底层抓包可以获取到也可以被覆盖
+    #                       document.cookie 获取不到cookie
+
+    #
